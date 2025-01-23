0fdd2c98dfdc18ee262b7ca8cf3807b1480eb659
Tyson Arnold <tarnol17@ut.utm.edu>
2025-01-23 14:32:14 -0600

    Ask for number

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+int sum(int n){
+  int total = 0;
+  for (int i = 1; i <= n; i++){
+    total += 1;
+  }
+  return total;
+}
+
 int main(){
-  cout << "Hello, World!" << endl;
+  int number;
+  cout << "Please give me a number";
+  cin >> number;
+
+  int total_number = sum(number);
+  cout << "Here is the sum of the number " << total_number << endl;
 }
