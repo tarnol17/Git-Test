3864dbbda221eda0d3926da9906e7408fa3ec0f8
Tyson Arnold <tarnol17@ut.utm.edu>
2025-01-23 14:52:05 -0600

    Product of number

--- main.cpp
+++ main.cpp
@@ -10,11 +10,22 @@ int sum(int n){
   return total;
 }
 
+int product(int n){
+  int total = 1;
+  for(int i = 1; i < n; i++){
+    result *= i;
+  }
+  return total;
+}
+
 int main(){
   int number;
   cout << "Please give me a number";
   cin >> number;
 
-  int total_number = sum(number);
-  cout << "Here is the sum of the number " << total_number << endl;
+  int total_sum = sum(number);
+  cout << "Here is the sum of the number " << total_sum << endl;
+
+  int total_product = product(number);
+  cout << "Here is the product of the number " << total_product << endl;
 }
