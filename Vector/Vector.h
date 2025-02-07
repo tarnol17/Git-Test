f69012931466800ac042dbc7a2ca1717e7e51d03
Tyson Arnold <tarnol17@ut.utm.edu>
2025-02-07 10:19:28 -0600

    Second commit: Vector.h updated

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,1 +1,32 @@
-hi
+/**
+ * @file Vector.h
+ * @author Tyson Arnold
+ * @date 2025-02-07
+ * @brief Vector Header File
+ *
+ */
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+
+class Vector {
+ private:
+  int *vec_ptr;
+  int vec_size, vec_capacity;
+
+ public:
+  Vector();
+  Vector(const Vector &other);
+  ~Vector();
+  Vector& operator=(const Vector &other);
+
+  int size();
+  int capacity();
+  void push_back(int element);
+  void reserve(int n);
+  int& operator[](unsigned int index);
+};
+
+#endif
