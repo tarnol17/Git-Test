99ebfe24f407884dd50c559312852d91245d8411
Tyson Arnold <tarnol17@ut.utm.edu>
2025-03-06 16:04:14 -0600

    phonebook.h updated

--- HW2/phonebook.h
+++ HW2/phonebook.h
@@ -1,1 +1,33 @@
- 
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct Node {
+  std::string first_name;
+  std::string last_name;
+  std::string phone_number;
+  Node* next;
+};
+
+class Phonebook {
+public:
+  Phonebook();
+  ~Phonebook();
+
+  void add(const std::string& first, const std::string& last, const std:: string& phone)
+  void add(const std::string& first, const std::string& last, const std::string& phone);
+  void load(const std::string& filename);
+  void save(const std::string& filename);
+  void print();
+  std::string find_by_last(const std::string& last);
+  std::string find_by_phone(const std::string& phone);
+  void remove(const std::string& first, const std::string& last);
+
+private:
+    Node* head;
+};
+
+#endif
